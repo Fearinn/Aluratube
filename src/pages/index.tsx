fe03dd863d2eddb0911641fc2821b3172a5f7a66
Matheus <matheusmgomes09@gmail.com>
2022-11-08 22:19:39 -0300

    favicon and home title added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
 import config from "data/config.json";
-import styled from "styled-components";
 import { CSSReset } from "components/CSSreset";
 import Menu from "components/Menu/index";
 import Timeline from "components/Timeline";
 import { useState } from "react";
 import Header from "components/Header";
 import Favorites from "components/Favorites";
+import Head from "next/head"
 
 function HomePage() {
   const [search, setSearch] = useState("");
   return (
     <>
+    <Head>
+      <title>Home - AluraTube</title>
+      <link rel="icon" type="image/x-icon" href="https://icons.iconarchive.com/icons/dakirby309/simply-styled/128/YouTube-icon.png"></link>
+    </Head>
       <CSSReset />
       <div
         style={{
