4435bd27539e0258d2b6bc48991c54d6759f2841
Fearinn <matheusmgomes09@gmail.com>
2023-02-20 14:33:23 -0300

    refactor: supabase instance exported from services

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,16 +1,11 @@
-import { createClient } from "@supabase/supabase-js";
 import { IPlaylists, ITimeline, IVideo } from "interfaces/timeline";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import PlaylistsContext from "providers/PlaylistsContext";
 import { useContext, useEffect } from "react";
-import videoService from "services/video";
+import videoService, { supabase } from "services/video";
 import StyledTimeline from "./styles";
 
-const PROJECT_URL = "https://szaruafpiauzxitymguy.supabase.co";
-const PUBLIC_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6YXJ1YWZwaWF1enhpdHltZ3V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgyMjE4NDgsImV4cCI6MTk4Mzc5Nzg0OH0.t0Bfs0pKt2LtGwQb5BE9AB7OoK8hWkdsNs5wqvzHSak";
-const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
 
 export const possiblePlaylists = ["jogos", "esportes", "tecnologia", "outros"];
 
