b976b1983feffa01feb94b1549a901d9e25b9670
Fearinn <matheusmgomes09@gmail.com>
2023-02-20 14:33:46 -0300

    fix: Select onChange properly typed

--- src/components/VideoRegister/index.tsx
+++ src/components/VideoRegister/index.tsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
+import React from "react";
 import StyledRegisterVideo from "components/VideoRegister/styles";
 import getIdFromURL from "utils/getIdFromURL";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import { createClient } from "@supabase/supabase-js";
-
-const PROJECT_URL = "https://szaruafpiauzxitymguy.supabase.co";
-const PUBLIC_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6YXJ1YWZwaWF1enhpdHltZ3V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgyMjE4NDgsImV4cCI6MTk4Mzc5Nzg0OH0.t0Bfs0pKt2LtGwQb5BE9AB7OoK8hWkdsNs5wqvzHSak";
-const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { supabase } from "services/video";
 
 const useForm = <T,>(initialValues: T) => {
   const [values, setValues] = useState(initialValues);
 
-  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const changeHandler = (
+    event: React.ChangeEvent<HTMLInputElement> | SelectChangeEvent
+  ) => {
     setValues({
       ...values,
       [event.target.name]: event.target.value,
@@ -103,7 +107,6 @@ function VideoRegister() {
                       required
                       name="playlist"
                       value={registerForm.values.playlist}
-                      /* @ts-ignore */
                       onChange={registerForm.changeHandler}
                       label="Playlist"
                     >
